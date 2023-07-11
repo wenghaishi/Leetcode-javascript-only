27c5a3b4368a221d0ede25b401f70698bfe500fb
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-07-12 01:03:22 +0800

    added editor theme toggling function

--- src/components/MonacoEditor.jsx
+++ src/components/MonacoEditor.jsx
@@ -2,39 +2,73 @@ import Editor from "@monaco-editor/react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import * as Babel from "@babel/standalone";
 
 const MonacoEditor = (props) => {
   const [value, setValue] = useState();
   const [output, setOutput] = useState();
+  const [darkMode, setDarkMode] = useState(false)
   const handleEditorChange = (value) => {
     setValue(value);
   };
 
+  const editorOptions = {
+    selectOnLineNumbers: true,
+    fontSize: 13,
+    automaticLayout: true,
+  };
+  let theme = "light"
+  if (darkMode === true) {
+    theme = "vs-dark"
+  }
+
+  const handleToggleTheme = () => {
+    setDarkMode(prev => !prev)
+  }
+
   const handleRunCode = () => {
-    setOutput(eval(value));
+    try {
+      const transformedCode = Babel.transform(value, { presets: ["env"] }).code;
+      const output = eval(transformedCode);
+      setOutput(output);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const { id } = useParams();
-  const questions = props.questions
-  const currentQuestion = questions[id-1]
+  const questions = props.questions;
+  const currentQuestion = questions[id - 1];
 
   return (
     <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+      {/* nav component */}
+      <nav className="pb-4 mb-4 w-full border-b-2 flex flex-row justify-between">
+        <Link to="/" className="">
+          Home
+        </Link>
+        <button onClick={handleToggleTheme}>Toggle editor theme</button>
+      </nav>
+      {/* end of nav component */}
+
       <div className="flex flex-row">
         <div className="w-1/2 bg-slate-100 p-6">
           <h1 className=" text-xl font-bold ">{currentQuestion.title}</h1>
           <p className="text-left	 px-2 my-4">{currentQuestion.description}</p>
           <h2 className="text-left font-bold px-2 pb-2">Example</h2>
-          <p className="text-left display-linebreak px-2"> {currentQuestion.example}</p>
+          <p className="text-left display-linebreak px-2">
+            {currentQuestion.example}
+          </p>
         </div>
         <Editor
-          height="70vh"
+          height="66vh"
           width={`100%`}
           defaultLanguage="javascript"
           defaultValue={`${currentQuestion.code}`}
           value={value}
           onChange={handleEditorChange}
-          theme="oceanic-next"
+          theme={theme}
+          options={editorOptions}
         />
       </div>
 
@@ -43,11 +77,10 @@ const MonacoEditor = (props) => {
       </div>
       <button
         onClick={handleRunCode}
-        className="bg-gray-500 text-white py-4 px-8 mr-4 rounded-lg mt-4 hover:bg-gray-600"
+        className="bg-gray-500 text-white py-4 px-12 mr-4 rounded-lg mt-4 hover:bg-gray-600"
       >
         Run code
       </button>
-      <Link to="/" className="bg-gray-500 text-white py-4 px-8 mr-4 rounded-lg mt-4 hover:bg-gray-600">Home</Link>
     </div>
   );
 };
