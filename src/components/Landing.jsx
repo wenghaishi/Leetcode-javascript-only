c75e16473fe03c025fc2b51bfbab5c8a74712dfd
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-07-11 21:39:25 +0800

    completed questions component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -14,21 +14,31 @@ const Landing = () => {
     },
 
     {
-      id: 1,
+      id: 2,
       title: "Palindrone Number",
       descripton:
         "Given an integer x, return true if x is a palindrome, and false otherwise.",
-      example: ["Input: x = 121. Output: true. Explanation: 121 reads as 121 from left to right and from right to left."],
-      difficulty: "Easy"
+      example: [
+        "Input: x = 121. Output: true. Explanation: 121 reads as 121 from left to right and from right to left.",
+      ],
+      difficulty: "Easy",
     },
   ];
   return (
-    <div>
-      <h1 className="text-5xl font-bold text-gray-700">
-        LeetCode Javascript ONLY
-      </h1>
-      {questions.map((question) => <Question question={question} key={question.id}/>)}
-    </div>
+    <>
+      <div className="flex flex-row w-full items-center justify-center">
+        <h1 className="text-5xl font-bold text-gray-700 mr-4">
+          LeetCode Javascript ONLY
+        </h1>
+        <img src="/dog.svg" className="h-16" />
+      </div>
+
+      <div className="flex flex-col items-center mt-10">
+        {questions.map((question) => (
+          <Question question={question} key={question.id} />
+        ))}
+      </div>
+    </>
   );
 };
 export default Landing;
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,8 +1,26 @@
-const Question = () => {
+const Question = ({ question }) => {
+  const { id, title, difficulty } = question;
+
+  const difficultyClasses = {
+    Easy: "text-green-500",
+    Medium: "text-orange-400",
+    Hard: "text-red-500",
+  };
+
+  const color = difficultyClasses[difficulty];
+
+  const handleClick = () => {
+
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
-}
-export default Question
+    <button onClick={handleClick} className="flex flex-row w-4/6 h-20 m-2 bg-slate-300 items-center justify-between px-6 hover:bg-slate-200">
+      <div className="flex flex-row items-center">
+        <h1 className="text-2xl pr-4">{id}</h1>
+        <h1 className="text-xl">{title}</h1>
+      </div>
+      <h1 className={`text-xl ${color}`}>{difficulty}</h1>
+    </button>
+  );
+};
+export default Question;
