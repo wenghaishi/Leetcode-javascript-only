9c7098a9b78b3a369e994c88c60a66cd188a66ff
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-07-12 19:32:59 +0800

    added logout to userpage

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -3,7 +3,8 @@ import Profile from "./Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const UserPage = () => {
-  const { user } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
+  const { logout } = useAuth0();
   return (
     <>
       {/* nav component */}
@@ -12,13 +13,28 @@ const UserPage = () => {
           <img src="/code.png" className="h-10" />
         </Link>
         <div className="flex flex-row">
+          {isAuthenticated && (
+            <button
+              className="mr-4"
+              onClick={() =>
+                logout({
+                  logoutParams: {
+                    returnTo: "https://leetcode-javascript-only.vercel.app/",
+                  },
+                })
+              }
+            >
+              Log Out
+            </button>
+          )}
           <Profile />
         </div>
       </nav>
       {/* end of nav component */}
 
       <div>
-        <h1>welcome</h1>
+        <h1 className="text-2xl my-6">The profile page of {user.name}</h1>
+        <h1>Work in progress</h1>
       </div>
     </>
   );
